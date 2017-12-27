5b0be2fd35d8e2c7f601f6e3752ed90da97cdd2e
John Kim <thisisjohnnya@gmail.com>
2017-12-27 13:59:28 -0500

    save

---
+++ .neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'monster-gen'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};
